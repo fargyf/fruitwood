27a1252928c2651d9d196552331117126da10354
caok <caok1231@gmail.com>
2013-01-17 16:57:51 +0800

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Fruitwood::Application.config.secret_token = '4e052db08aeffccc7513145018c8e4636259f7d3a3926082d640e3a141b4d32456795911a60f1faaa0c383b6f875aff807651118cbd9c222864603ddef52bc9f'
