20d4f75736eb92f3f3b52710686fcc0d7a65e3e8
caok <caok1231@gmail.com>
2013-01-18 15:26:08 +0800

    add something into users and login by tel from email

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,14 @@
 Fruitwood::Application.routes.draw do
   devise_for :users
 
-  root to: "home#index"
+  authenticated :user do
+    root to: "home#index"
+  end
+
+  as :user do
+    root to: 'devise/registrations#new'
+  end
+
   get "home/index"
 
   # The priority is based upon order of creation:
