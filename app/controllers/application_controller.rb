fdb45ad78f414746c2da9bb4c4430f4068d9a15a
caok <caok1231@gmail.com>
2013-01-21 22:49:03 +0800

    add ability by cancan

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+
+  # cancan exception handler
+  rescue_from CanCan::AccessDenied do |exception|
+    exception.default_message = I18n.t("unauthorized.default") || exception.default_message
+    flash[:error] = exception.message
+    redirect_to root_url
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,6 @@
 class UsersController < ApplicationController
+  load_and_authorize_resource :only => [:show, :index]
+
   def show
     @user = User.find(params[:id])
   end
