ac25ad1745165eeafe894e5564f0adb883d8d0ab
caok <caok1231@gmail.com>
2013-01-22 23:30:54 +0800

    add html_pipeline to evnents

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,5 +1,9 @@
 class Event < ActiveRecord::Base
+  extend HasHtmlPipeline
+
   attr_accessible :content, :end_date, :start_date, :title
+
+  has_html_pipeline :content, :markdown
 
   validates :title, :start_date, :end_date, :presence => true
 end
